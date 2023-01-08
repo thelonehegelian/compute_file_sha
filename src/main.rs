80b5b0a1bc5faf93f036dc23b1784fb21c8ad7fe
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-08 05:02:58 +0000

    Fix value moved in closure error
    clone the sender inside of the loop but before the thread execution. The problem was that the sender was running out of scope after each iteration

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use num_cpus;
 use ring::digest::{Context, Digest, SHA256};
 use std::fs::File;
 use std::io::{BufReader, Error, Read};
-use std::{io::BufRead, path::Path};
+use std::path::Path;
+use std::sync::mpsc::channel;
+use threadpool::ThreadPool;
 use walkdir::WalkDir;
-
 // filepath is of type P and P is a type that must implement the AsRef trait
 fn compute_digest<P: AsRef<Path>>(filepath: P) -> Result<Digest, Error> {
     // open file using a buffer
@@ -28,10 +30,46 @@ fn compute_digest<P: AsRef<Path>>(filepath: P) -> Result<Digest, Error> {
 }
 
 fn main() {
+    // TODO: use threads
+    // num_cpus gets the number of threads on a system
+    let num_cpus = num_cpus::get();
+    // create a pool of threads
+    let pool = ThreadPool::new(num_cpus);
+    // channel will send and receive a Digest type
+    let (sender, receiver) = channel();
+
     let folder_to_walk = "Midjourney";
     // walk through the directory and compute digest
-    for entry in WalkDir::new(&folder_to_walk).min_depth(1) {
-        let digest = compute_digest(entry.unwrap().path()).unwrap();
-        println!("{:?}", digest);
+    for entry in WalkDir::new(&folder_to_walk).max_depth(1) {
+        let sender = sender.clone();
+        let path = entry.unwrap().path().to_owned();
+        pool.execute(move || {
+            let digest = compute_digest(path).unwrap();
+            // send the computed digest through the channel
+            sender.send(digest).expect("Crashed");
+        });
     }
+    drop(sender);
+
+    for msg in receiver {
+        println!("{:?}", msg);
+    }
+    // for entry in WalkDir::new("Midjourney")
+    //     .follow_links(true)
+    //     .into_iter()
+    //     .filter_map(|e| e.ok())
+    //     .filter(|e| !e.path().is_dir())
+    // {
+    //     let path = entry.path().to_owned();
+    //     let sender = sender.clone();
+    //     pool.execute(move || {
+    //         let digest = compute_digest(path);
+    //         sender.send(digest).expect("Could not send data!");
+    //     });
+    // }
+    // drop(sender);
+    // for t in receiver.iter() {
+    //     let sha = t.unwrap();
+    //     println!("{:?}", sha);
+    // }
 }
